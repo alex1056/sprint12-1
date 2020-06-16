d267512395822a51ecd64ab99dc833a021014987
alexander.b <gituser1056@gmail.com>
2020-06-16 23:02:05 +0300

    Убрал нод модулес

--- helpers/read-file.js
+++ helpers/read-file.js
@@ -5,25 +5,11 @@ async function readFile(pathToFile) {
     let stringData = await fsPromises.readFile(pathToFile, { encoding: 'utf8' });
     let objData = await JSON.parse(stringData);
     return objData;
-    //console.log(objData);
-  } catch (error) {
+  }
+  catch (error) {
     console.log(error);
   }
 }
-// async function readFile(pathToFile) {
-//   return await fsPromises.readFile(pathToFile, { encoding: 'utf8' })
-//     .then((data) => {
-//       return JSON.parse(data)
-//     })
-//     // .then((data) => {
-//     //   // console.log(data);
-//     //   return data;
-//     // })
-//     .catch(err => {
-//       console.log(err);
-//     });
-// }
-
 module.exports = {
   readFile
 };
